3bb1ffb0f60ea44445f1da4f6d0a3bb9abc8f7ac
Simon Waldherr <git@simon.waldherr.eu>
2013-06-04 00:27:26 +0200

    better test added (check against php hash();)

--- hash.php
+++ hash.php
@@ -3,21 +3,18 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Headers: *");
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
 header('X-Powered-By:0xBADCAB1E');
 
-switch ($_GET['hash']) {
-  case 'whirlpool':
-    echo hash("whirlpool", utf8_decode($_GET['string']));
-    break;
-  case "sha512":
-    echo hash("sha512", utf8_decode($_GET['string']));
-    break;
-  case "md5":
-    echo hash("md5", utf8_decode($_GET['string']));
-    break;
-  case "crc32":
-    echo hash("crc32", utf8_decode($_GET['string']));
-    break;
-}
+$hash['_algos']     = array('whirlpool', 'sha512', 'md5', 'crc32');
+$hash['_string']    = $_GET['string'];
+$hash['whirlpool'] = hash("whirlpool", utf8_decode($hash['_string']));
+$hash['sha512']    = hash("sha512", utf8_decode($hash['_string']));
+$hash['md5']       = hash("md5", $hash['_string']);
+$hash['crc32']     = hash("crc32", utf8_decode($hash['_string']));
+
+echo json_encode($hash);
 
 ?>
